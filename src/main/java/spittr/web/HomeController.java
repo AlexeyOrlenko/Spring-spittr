705db60dc55655593cc15bddded60117fb400a07
Yuriy Kuzmenko <ykuzmenko73@gmail.com>
2016-03-31 21:52:56 +0600

    базовый пример заработал в tomcat

--- src/main/java/spittr/web/HomeController.java
+++ src/main/java/spittr/web/HomeController.java
@@ -2,13 +2,14 @@ package spittr.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import static org.springframework.web.bind.annotation.RequestMethod.*;
 
 /** @author yvkuzmen */
 @Controller
+@RequestMapping(value = "/")
 public class HomeController {
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @RequestMapping(method = GET)
     public String home(){
         return "home";
     }
